69b7ae5413a1482f010daaf6da91f3aacb50f565
AndreZ <zwieback@crazyuniverse.net>
2024-11-17 17:00:54 +0100

    updated premake file

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,6 @@
 project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
-	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -27,12 +25,14 @@ project "yaml-cpp"
 
 	filter "system:windows"
 		systemversion "latest"
-
+		cppdialect "C++17"
+		staticruntime "off"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-
+		cppdialect "C++17"
+		staticruntime "off"
 
 	filter "configurations:Debug"
 		runtime "Debug"
